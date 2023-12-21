88ed05dc403a06298dd1b0f5cc79790720b83804
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2023-12-21 18:43:36 +0100

    add dragndrop and use gesture library

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-import { DropArea } from "../components/DropArea";
-import Register from "../components/Register";
+import { useState } from 'react';
+import { useDrag } from '@use-gesture/react';
 import { Instructions } from "../components/Instructions";
-import { Elements } from "../components/Elements";
-import { DnMFunc } from "../components/DnMFunc";
+import profileFrame from '../components/Images/profileFrame.png';
 
 
 export const GamePage = () => {
+  const [elementPos, setElementPos] = useState({ x: 0, y: 0});
+  const bindElementPos= useDrag((params) => {
+setElementPos({
+x:params.offset[0],
+y:params.offset[1],
+  });
+});
   return ( 
   <div style={{marginTop: "25px"}}>
-    <DnMFunc />
+    <div {...bindElementPos()} style={{
+        position: 'relative',
+        top: elementPos.y,
+        left: elementPos.x,
+        touchAction: 'none',
+      }}>
+         <img className="profileFrame" src={profileFrame}/>
+      </div>
+
     <Instructions/>
-    <DropArea/>
+   
   </div>
 )};
 
